3d8b031bf79228a47067c0f6cffe5ed87cd27d31
Vladislav <barabayne@gmail.com>
2024-11-07 09:17:52 +0500

    fix: delete extra metadata

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,11 +1,9 @@
 import uuid
 
-from sqlalchemy import MetaData
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, as_declarative, mapped_column
 
 
 @as_declarative()
 class BaseModel:
-    metadata = MetaData()
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)

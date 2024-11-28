29c01b96f19567d5941258aa625298b568ae11bb
Vladislav <barabayne@gmail.com>
2024-11-29 00:22:50 +0500

    tests: update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,32 @@
 import asyncio
 
-import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+
+from src.config import settings
+from src.database.database import engine
+from src.database.models.base import BaseModel
+from src.main import app
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest_asyncio.fixture(scope='session', autouse=True)
 def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def prepare_database():
+    assert settings.MODE == 'TEST'
+
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
+        await conn.run_sync(BaseModel.metadata.create_all)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def async_client():
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        yield client
+        client.aclose()

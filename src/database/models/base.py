b44259830e7d807ecc7fd4eff3e6ff9c198f2b7f
Vladislav <barabayne@gmail.com>
2025-01-06 12:00:08 +0500

    chore: alembic name convention

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,9 +1,25 @@
 import uuid
 
+from sqlalchemy import MetaData
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, as_declarative, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+convention = {
+    'all_column_names': lambda constraint, table: '_'.join(
+        [column.name for column in constraint.columns.values()]
+    ),
+    'ix': 'ix__%(table_name)s__%(all_column_names)s',
+    'uq': 'uq__%(table_name)s__%(all_column_names)s',
+    'ck': 'ck__%(table_name)s__%(constraint_name)s',
+    'fk': 'fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s',
+    'pk': 'pk__%(table_name)s',
+}
 
 
-@as_declarative()
-class BaseModel:
+class Base(AsyncAttrs, DeclarativeBase):
+    metadata = MetaData(naming_convention=convention)
+
+
+class BaseModel(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)

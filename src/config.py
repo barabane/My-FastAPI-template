7201d3c7c912e509f45633fbcb94a71d96202bfc
Vladislav <barabayne@gmail.com>
2025-01-07 10:42:39 +0500

    chore: add infisical

--- src/config.py
+++ src/config.py
@@ -1,32 +1,61 @@
 from typing import Literal
 
+from infisical_client import ClientSettings, GetSecretOptions, InfisicalClient
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class CustomBaseSettings(BaseSettings):
+class Base(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env')
 
+    INFISICAL_ID: str
+    INFISICAL_SECRET: str
+    PROJECT_ID: str
 
-class Settings(CustomBaseSettings):
-    MODE: Literal['TEST', 'PROD', 'DEV'] = 'DEV'
 
-    POSTGRES_NAME: str
-    POSTGRES_PASS: str
-    POSTGRES_USER: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
+base_settings = Base()
 
-    TEST_POSTGRES_NAME: str
-    TEST_POSTGRES_USER: str
-    TEST_POSTGRES_PASS: str
-    TEST_POSTGRES_HOST: str
-    TEST_POSTGRES_PORT: int
+client = InfisicalClient(
+    ClientSettings(
+        client_id=base_settings.INFISICAL_ID,
+        client_secret=base_settings.INFISICAL_SECRET,
+    )
+)
 
-    DB_POOL_SIZE: int
-    DB_POOL_SIZE_MAX_OVERFLOW: int
-    DB_POOL_TTL: int
-    DB_POOL_PRE_PING: bool
+
+def get_infisical_secret(secret_name: str, environment: str = 'dev'):
+    return client.getSecret(
+        options=GetSecretOptions(
+            environment=environment,
+            project_id=base_settings.PROJECT_ID,
+            secret_name=secret_name,
+        )
+    ).secret_value
+
+
+class Settings:
+    MODE: Literal['TEST', 'PROD', 'DEV'] = get_infisical_secret(secret_name='MODE')
+
+    POSTGRES_NAME: str = get_infisical_secret(secret_name='POSTGRES_NAME')
+    POSTGRES_PASS: str = get_infisical_secret(secret_name='POSTGRES_PASS')
+    POSTGRES_USER: str = get_infisical_secret(secret_name='POSTGRES_USER')
+    POSTGRES_HOST: str = get_infisical_secret(secret_name='POSTGRES_HOST')
+    POSTGRES_PORT: int = int(get_infisical_secret(secret_name='POSTGRES_PORT'))
+
+    TEST_POSTGRES_NAME: str = get_infisical_secret(secret_name='TEST_POSTGRES_NAME')
+    TEST_POSTGRES_USER: str = get_infisical_secret(secret_name='TEST_POSTGRES_USER')
+    TEST_POSTGRES_PASS: str = get_infisical_secret(secret_name='TEST_POSTGRES_PASS')
+    TEST_POSTGRES_HOST: str = get_infisical_secret(secret_name='TEST_POSTGRES_HOST')
+    TEST_POSTGRES_PORT: int = int(
+        get_infisical_secret(secret_name='TEST_POSTGRES_PORT')
+    )
+
+    DB_POOL_SIZE: int = int(get_infisical_secret(secret_name='DB_POOL_SIZE'))
+    DB_POOL_SIZE_MAX_OVERFLOW: int = int(
+        get_infisical_secret(secret_name='DB_POOL_SIZE_MAX_OVERFLOW')
+    )
+    DB_POOL_TTL: int = int(get_infisical_secret(secret_name='DB_POOL_TTL'))
+    DB_POOL_PRE_PING: bool = get_infisical_secret(secret_name='DB_POOL_PRE_PING')
 
     DATABASE_URL: PostgresDsn | None = None
     TEST_DATABASE_URL: PostgresDsn | None = None
